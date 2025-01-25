8bf95f7fdd18a4cb5446b31d24c590c8802c462e
Asingura Philip <asinguraphilip@gmail.com>
2025-01-25 12:19:22 +0300

    functions

---
+++ backend/functions.js
@@ -0,0 +1,22 @@
+//void function
+function name(){
+    console.log()
+}
+
+//returning function
+function name(){
+    console.log()
+}
+
+//arrow functions
+
+let add = ()=>{
+    console.log()
+}
+
+let user = "henry"
+function greet(name){
+    console.log(`hello ${name}`)
+}
+
+greet(user)
