773ee8f411383f73dc9f23c59e44c19694e31643
Asingura Philip <asinguraphilip@gmail.com>
2025-02-10 18:43:37 +0300

    changes

--- backend/aysnc.js
+++ backend/aysnc.js
@@ -1,4 +1,11 @@
+// synchronous programming
+
+//boiling water
 console.log('pour water into kettle')
 console.log('put kettle on stove')
 console.log('turn on stove')
 console.log('water boils')
+//timer
+setTimeout(()=>{
+    console.log('water is ready')
+},5000)
