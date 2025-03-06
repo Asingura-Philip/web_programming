874616777dbde0b45ec25320138d24d3f8c45635
Asingura Philip <asinguraphilip@gmail.com>
2025-03-06 12:05:03 +0300

    sql routes

--- backend/helloserver/server.js
+++ backend/helloserver/server.js
@@ -1,20 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const db = require("./config");
 
 const port = 3000;
 
-app.get('/hello',(req,res)=>{
-    res.send("hello world")
-})
+app.use(express.json());
+
+const sql = `CREATE TABLE IF NOT EXISTS users(
+            id int AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(15),
+            email VARCHAR(10)
+)`;
+
+db.query(sql, (err, result) => {
+  if (err) throw err;
+  console.log("table created");
+});
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
 
 // function requestLogger(req,res,next){
 //     console.log(`request method: ${req.method},URL: ${req.url}`)
 //     next();
 // }
-app.get('*',(req,res)=>{
-    res.send('this route does not exist')
-})
 
-app.listen(port,()=>{
-    console.log(`the server is running on ${port}`)
-})
+
+app.get("*", (req, res) => {
+  res.send("this route does not exist");
+});
+
+app.listen(port, () => {
+  console.log(`the server is running on ${port}`);
+});
